f3a67b11f4fd61378e3017b03060001b5219e36b
Dana Kuglin <dakuglin@gmail.com>
2020-12-15 18:54:31 -0700

    added styling elements

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -3,10 +3,14 @@ import "./hero.css";
 
 function Hero(props) {
   return (
-    <div className="hero img-fluid" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
-      {props.children}
-      <h1 className="hero-text">Oh, hello.</h1>
-      <h1 className="hero-text">Nice to meet you.</h1>
+    <div className="hero-container">
+      <div className="hero-img img-fluid" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+        {props.children}
+        <div className="hero-overlay">
+          <h1> Oh, hello.</h1>
+          <h1>Nice to meet you.</h1>
+        </div>
+      </div>
     </div>
   );
 };
